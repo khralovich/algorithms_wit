81b933d7558d3c37146fc3bb91dd3bea6c6d7e40
khralovich <khralovich@protonmail.com>
2024-01-14 20:48:43 +0100

    init

---
+++ sum_of_sets.py
@@ -0,0 +1,5 @@
+"""
+Given two arrays A and B (n-element and m-element) with elements that are natural numbers. The arrays are in ascending order.
+Construct an algorithm to determine the number of elements belonging to AUB (sum of sets, ie. each element can occur once).
+E.g. A = {1, 2, 2, 3, 3, 5, 8}, B = {1, 3, 4, 8, 9, 10}, AUB = {1, 2, 3, 4, 5, 8, 9, 10}, so the number you are looking for is 8.
+"""
